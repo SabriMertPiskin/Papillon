93612aaad4e092b9dad4dfda49956672e65d0d99
Mete Cem Turan <metecemturan02@gmail.com>
2026-04-07 15:10:35 +0300

    Update aws_service.py

--- backend/papillon/vm_lab/aws_service.py
+++ backend/papillon/vm_lab/aws_service.py
@@ -25,3 +25,29 @@ def start_ec2_instance(instance_id, region=REGION):
         "status": state,
         "message": "instance starting"
     }
+    
+# import boto3
+# import time
+
+
+# def start_ec2_instance(instance_id, region="eu-central-1"):
+#     ec2 = boto3.client("ec2", region_name=region)
+
+#     # instance start
+#     ec2.start_instances(InstanceIds=[instance_id])
+
+#     # IP hemen gelmeyebilir, biraz beklemek gerekir
+#     time.sleep(10)
+
+#     response = ec2.describe_instances(InstanceIds=[instance_id])
+
+#     instance = response["Reservations"][0]["Instances"][0]
+
+#     public_ip = instance.get("PublicIpAddress", None)
+#     state = instance["State"]["Name"]
+
+#     return {
+#         "instance_id": instance_id,
+#         "status": state,
+#         "public_ip": public_ip
+#     }
